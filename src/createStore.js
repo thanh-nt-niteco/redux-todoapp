e738f2bbc4a5f01b4cfc0042f6c4b00134ed6614
thanh nt <thanh.nt@niteco.se>
2018-10-25 16:43:10 +0700

    Use throttle function from lodash to reduce the call of saving data to localstorage

--- src/createStore.js
+++ src/createStore.js
@@ -1,17 +1,17 @@
 
 import {createStore} from 'redux';
 import Reducer from './reducers';
-
 import {loadState, saveState} from './services/localStorage';
+import throttle from 'lodash/throttle';
 
 const persistedState = loadState();
 const store = createStore(Reducer, persistedState);
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
     saveState({
         todos: store.getState().todos
     });
-});
+}, 1000));
 
 export default function() {
     return store;
