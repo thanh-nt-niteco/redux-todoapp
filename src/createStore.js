6ce47e77273fced964e3cf49fdd1f3a3e02201a8
thanh nt <thanh.nt@niteco.se>
2018-10-26 17:06:40 +0700

    Wrapping dispatch with a logging util

--- src/createStore.js
+++ src/createStore.js
@@ -4,15 +4,34 @@ import Reducer from './reducers';
 import {loadState, saveState} from './services/localStorage';
 import throttle from 'lodash/throttle';
 
-const persistedState = loadState();
-const store = createStore(Reducer, persistedState);
+const addLoggingToDispatch = function(store) {
+    const rawDispatch = store.dispatch;
+    if(!console.group) {
+        return rawDispatch;
+    }
 
-store.subscribe(throttle(() => {
-    saveState({
-        todos: store.getState().todos
-    });
-}, 1000));
+    return function(action) {
+        console.group(action.type);
+        console.log('%c previous state', 'color: blue', store.getState());
+        console.log('%c action', 'color: red', action);
+        const returnValue = rawDispatch(action);
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    };
+}
 
 export default function() {
+    const persistedState = loadState();
+    const store = createStore(Reducer, persistedState);
+
+    store.dispatch = addLoggingToDispatch(store);
+
+    store.subscribe(throttle(() => {
+        saveState({
+            todos: store.getState().todos
+        });
+    }, 1000));
+
     return store;
 }
